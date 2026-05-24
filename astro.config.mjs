2b99010c28fd7ee7ff4db8d5d2f0cd285b80baa0
Local Dev <local@dev>
2026-05-24 12:25:37 +0800

    switch adapter: @astrojs/node -> @astrojs/vercel, npm 管理依赖

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
+  adapter: vercel({
+    webAnalytics: { enabled: false },
   }),
   vite: {
     ssr: {
