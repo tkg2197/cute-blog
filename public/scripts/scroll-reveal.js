7af036bef1970825c630b476bbae7f8d027ce4ce
tkg <2197322347@qq.com>
2026-05-24 18:30:25 +0800

    首页布局调整

--- public/scripts/scroll-reveal.js
+++ public/scripts/scroll-reveal.js
@@ -6,8 +6,8 @@
   "use strict";
 
   var SELECTORS = [
-    ".home-about", ".home-modules", ".home-posts",
-    ".home-module-list a", ".home-post-list a",
+    ".home-about", ".home-posts",
+    ".home-post-list a",
     ".rec-header", ".rec-filter", ".rec-timeline > *",
     ".pw-header", ".pw-toolbar", ".pw-stacks > *", ".pw-detail__bar", ".pw-grid > *",
     ".bi-head", ".bi-toolbar", ".post-item", ".tag-panel", ".archive-panel", ".bi-pager",
